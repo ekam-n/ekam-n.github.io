a8c77297944694b5c046a068e40cb33591fdf81c
Ekam Nijjar <ekamnijjar@gmail.com>
2024-10-10 15:04:12 -0700

    added bases of first and second visualizations

--- assignment3.js
+++ assignment3.js
@@ -5,19 +5,40 @@ async function render() {
   const data = await d3.csv("./datasets/videogames_long.csv");
 
   // create a bar chart
-  const vlSpec = vl
+  const vl1 = vl
     .markBar()
     .data(data)
     .encode(
-      vl.y().fieldN("platform").sort("-x"),
-      vl.x().fieldQ("global_sales").aggregate("sum")
+      vl.x().fieldN("genre").title("Genre"),
+      vl.y().fieldQ("global_sales").aggregate("sum").title("Global Sales"), 
+      vl.facet().fieldN("platform").title("Platform"), // facet the graph by platform
+      vl.color().fieldN("genre")
+      .scale({scheme: "category20"}) // make each genre a different color
     )
-    .width("container")
-    .height(400)
+    // .width("container")
+    // .width(1000)
+    .height(500)
     .toSpec();
 
-  const view = await vegaEmbed("#a3-vis1", vlSpec).view;
-  view.run();
+  const view1 = await vegaEmbed("#a3-vis1", vl1).view;
+  // view1.run();
+
+  // create an area graph
+  const vl2a = vl
+  .markArea()
+  .data(data)
+  .encode(
+    vl.x().fieldN("year").title("Year").axis({format: "d"}), 
+    vl.y().aggregate("sum").fieldQ("global_sales").title("Global Sales"), 
+    vl.color().fieldN("genre").scale({scheme: "category20"}).title("Genre")
+  )
+
+  .height(500)
+  .toSpec();
+
+  const view2 = await vegaEmbed("#a3-vis2a", vl2a).view
+  // view2.run()
+    
 }
 
 render();
