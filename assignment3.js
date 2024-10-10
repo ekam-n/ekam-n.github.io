27260ec19eb1dc94b041937ad28dc27b68f93391
Ekam Nijjar <ekamnijjar@gmail.com>
2024-10-10 15:33:40 -0700

    added all bases of graphs

--- assignment3.js
+++ assignment3.js
@@ -36,8 +36,138 @@ async function render() {
   .height(500)
   .toSpec();
 
-  const view2 = await vegaEmbed("#a3-vis2a", vl2a).view
+  const view2a = await vegaEmbed("#a3-vis2a", vl2a).view
   // view2.run()
+
+  const vl2b = vl
+  .markRect()
+  .data(data)
+  .encode(
+    vl.x().fieldN("year").title("Year").axis({format: "d"}), 
+    vl.y().fieldN("platform").title("Platform"), 
+    vl.color()
+      .aggregate("sum").fieldQ("sales_amount")
+      .scale({ scheme: 'blues' }) // set color scale to 'blues'
+      .title("Total Sales (in millions)"), // title for the color legend
+  )
+
+  .height(500)
+  .toSpec();
+
+  const view2b = await vegaEmbed("#a3-vis2b", vl2b);
+
+  const vl3 = vl
+  .markBar()
+  .data(data)
+  .encode(
+    vl.x().fieldN("platform").title("Platform"), // make the genre the x field
+    vl.y().aggregate("sum").fieldQ("global_sales").title("Global Sales"), // make the global sales the y field, aggregated to a sum
+    vl.facet().fieldN("sales_region").title("Region"), // facet the graph by platform
+    vl.color().fieldN("platform").scale({scheme: "category20"}) // make each genre a different color
+  )
+  .height(500)
+  .toSpec();
+
+  const view3 = await vegaEmbed("#a3-vis3", vl3);
+
+  const vl4a = vl
+  .markLine()
+  .data(data)
+  .transform(
+    vl.filter('datum.sales_region === "jp_sales"')
+  )
+  .encode(
+    vl.x().fieldQ("year").title("Years").axis({format: "d"}), 
+    vl.y().aggregate("sum").fieldQ("sales_amount").title("Sales"), 
+    vl.color()
+    .fieldN("genre")
+    .scale({range: ["#bbbbbb"]})
+    .condition(
+      { 
+        test: 'datum.genre === "Role-Playing"',
+        value: "red"
+      }
+     )
+    .legend(null), 
+    vl.opacity() 
+      .condition(
+        {
+          test: 'datum.genre !== "Role-Playing"', 
+          value: 0.5
+        }
+      )
+  )
+  .height(500)
+  .width(1000)
+  .toSpec();
+
+  const view4a = await vegaEmbed("#a3-vis4a", vl4a);
+
+  const vl4b = vl
+  .markLine()
+  .data(data)
+  .transform(
+    vl.filter('datum.sales_region === "jp_sales"')
+  )
+  .encode(
+    vl.x().fieldQ("year").title("Years").axis({format: "d"}), 
+    vl.y().aggregate("sum").fieldQ("sales_amount").title("Sales"), 
+    vl.color()
+    .fieldN("genre")
+    .scale({range: ["#bbbbbb"]})
+    .condition(
+      { 
+        test: 'datum.genre === "Misc"',
+        value: "green"
+      }
+     )
+    .legend(null), 
+    vl.opacity() 
+      .condition(
+        {
+          test: 'datum.genre !== "Misc"', 
+          value: 0.5
+        }
+      )
+  )
+  .height(500)
+  .width(1000)
+  .toSpec();
+
+  const view4b = await vegaEmbed("#a3-vis4b", vl4b);
+
+  const vl4c = vl
+  .markLine()
+  .data(data)
+  .transform(
+    vl.filter('datum.sales_region === "jp_sales"')
+  )
+  .encode(
+    vl.x().fieldQ("year").title("Years").axis({format: "d"}), 
+    vl.y().aggregate("sum").fieldQ("sales_amount").title("Sales"), 
+    vl.color()
+    .fieldN("genre")
+    .scale({range: ["#bbbbbb"]})
+    .condition(
+      { 
+        test: 'datum.genre === "Action"',
+        value: "blue"
+      }
+     )
+    .legend(null), 
+    vl.opacity() 
+      .condition(
+        {
+          test: 'datum.genre !== "Action"', 
+          value: 0.5
+        }
+      )
+  )
+  .height(500)
+  .width(1000)
+  .toSpec();
+
+  const view4c = await vegaEmbed("#a3-vis4c", vl4c);
     
 }
 
