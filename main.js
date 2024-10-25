7f7c361c03eaa79436aee651cce58cc86b20dae1
ekam-n <125761106+ekam-n@users.noreply.github.com>
2024-10-25 00:14:04 -0700

    added image wheels for projects

--- main.js
+++ main.js
@@ -25,3 +25,27 @@ const assignment3Button = document.querySelector('#assignment3');
 assignment3Button.addEventListener('click', () => {
     window.location.href = 'assignment3.html'; // Redirect to assignment3.html
 });
+
+// Image sources for each project
+const images = {
+    'volt-legacy': ['images/volt1.png', 'images/volt2.png', 'images/volt3.png', 'images/volt4.png', 'images/volt5.png'],
+    'controlled-chaos': ['images/chaos1.jpg', 'images/chaos2.png', 'images/chaos3.jpg']
+};
+
+// Counters to track the current image for each project
+const imageCounters = {
+    'volt-legacy': 0,
+    'controlled-chaos': 0
+};
+
+// Function to show the previous image
+function prevImage(project) {
+    imageCounters[project] = (imageCounters[project] - 1 + images[project].length) % images[project].length;
+    document.getElementById(`${project}-img`).src = images[project][imageCounters[project]];
+}
+  
+// Function to show the next image
+function nextImage(project) {
+    imageCounters[project] = (imageCounters[project] + 1) % images[project].length;
+    document.getElementById(`${project}-img`).src = images[project][imageCounters[project]];
+}
